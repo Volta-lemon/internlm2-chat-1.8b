75089ac3a6a0ad07b3d00a11f1c9e86049b5504a
Volta-lemon <83173362+Volta-lemon@users.noreply.github.com>
2024-04-24 17:12:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import gradio as gr
 import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
-from openxlab.model import download
 
-base_path = './internlm2-chat-7b'
-os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+# download internlm2 to the base_path directory using git tool
+base_path = './final_model'
+os.system(f'git clone https://code.openxlab.org.cn/volta/xtuner_interml_1.8b.git {base_path}')
+
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
@@ -16,8 +17,8 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-7B",
+                 title="xtuner_interml_1.8b",
                 description="""
-InternLM is mainly developed by Shanghai AI Laboratory.  
+微调1.8b模型
                  """,
                  ).queue(1).launch()
