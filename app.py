dc28a2171c8ad3e5ecaaccaac5efd5f657dcf0ef
Volta-lemon <83173362+Volta-lemon@users.noreply.github.com>
2024-04-24 17:57:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,49 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './final_model'
-os.system(f'git clone https://code.openxlab.org.cn/volta/xtuner_interml_1.8b.git {base_path}')
+base_path = './internlm2-chat-1_8b-4bit'
+os.system(f'git clone https://code.openxlab.org.cn/volta/internlm2-chat-1_8b-4bit.git {base_path}')
 
 os.system(f'cd {base_path} && git lfs pull')
 
-tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+# tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+# model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
-def chat(message,history):
-    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
-        yield response
+# def chat(message,history):
+#     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+#         yield response
 
-gr.ChatInterface(chat,
-                 title="xtuner_interml_1.8b",
-                description="""
-微调1.8b模型
-                 """,
-                 ).queue(1).launch()
+# gr.ChatInterface(chat,
+#                  title="xtuner_interml_1.8b",
+#                 description="""
+# 微调1.8b模型
+#                  """,
+#                  ).queue(1).launch()
+# 初始化 pipeline
+pipe = pipeline(
+    base_path=base_path,
+    backend_config=backend_config
+)
+
+# 设置生成配置
+gen_config = GenerationConfig(
+    top_p=0.8,
+    top_k=40,
+    temperature=0.8,
+    max_new_tokens=1024
+)
+
+# 定义聊天功能
+def chat(message, history):
+    response = pipe(
+        message,
+        gen_config=gen_config
+    )
+    return response.text
+
+# 设置 gradio 聊天界面
+gr.ChatInterface(
+    fn=chat,
+    title="InternLM2-Chat-1.8_4bit,
+    description="""InternLM is mainly developed by Shanghai AI Laboratory."""
+).queue(1).launch()
